f9c566cd543ef0c91c6b5090195cb0ec8161adeb
Quiyet Brul <qbrul0514@gmail.com>
2023-07-26 00:20:22 -0600

    Update Activity_BubbleSort.go

--- 2-FunctionsMethodsAndInterfacesInGo/module1/Activity_BubbleSort.go
+++ 2-FunctionsMethodsAndInterfacesInGo/module1/Activity_BubbleSort.go
@@ -8,13 +8,29 @@ import (
 	"strings"
 )
 
-func bubbleSort(numbers []int) {
-	n := len(numbers)
+func main() {
+	numbers := getUserInput()
+	if numbers == nil {
+		return
+	}
+
+	fmt.Println("Unsorted array:", numbers)
+	bubbleSort(numbers)
+	fmt.Println("Sorted array:", numbers)
+}
+
+func bubbleSort(arr []int) {
+	n := len(arr)
 	for i := 0; i < n-1; i++ {
+		swapped := false
 		for j := 0; j < n-i-1; j++ {
-			if numbers[j] > numbers[j+1] {
-				swap(numbers, j)
+			if arr[j] > arr[j+1] {
+				swap(arr, j)
+				swapped = true
 			}
+		}
+		if !swapped {
+			break
 		}
 	}
 }
@@ -29,36 +45,34 @@ func readInputLine() string {
 	return strings.TrimSpace(input)
 }
 
-func getUserInput() []int {
-	fmt.Println("Enter up to 10 integers (separated by spaces): ")
-	input := readInputLine()
-
+func convertToIntegers(input string) ([]int, error) {
 	numStrings := strings.Fields(input)
 	numbers := make([]int, 0, len(numStrings))
 	for _, numStr := range numStrings {
 		num, err := strconv.Atoi(numStr)
 		if err != nil {
-			fmt.Println("Invalid input. Please enter valid integers.")
-			return nil
+			return nil, fmt.Errorf("invalid input: please enter valid integers")
 		}
 		numbers = append(numbers, num)
 	}
 
 	if len(numbers) > 10 {
 		fmt.Println("Input contains more than 10 integers. Please try again.")
-		return getUserInput()
+		numbers = getUserInput()
+	}
+
+	return numbers, nil
+}
+
+func getUserInput() []int {
+	fmt.Println("Enter up to 10 integers (separated by spaces): ")
+	input := readInputLine()
+
+	numbers, err := convertToIntegers(input)
+	if err != nil {
+		fmt.Println(err)
+		return nil
 	}
 
 	return numbers
-}
-
-func main() {
-	numbers := getUserInput()
-	if numbers == nil {
-		return
-	}
-
-	fmt.Println("Unsorted array:", numbers)
-	bubbleSort(numbers)
-	fmt.Println("Sorted array:", numbers)
 }
